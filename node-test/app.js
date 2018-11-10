572eb9da3265e03de6af65d42e0bae735ed0bc28
Edwards Presume <edwardspresume@aol.com>
2018-11-09 20:28:25 -0500

    Update

--- node-test/app.js
+++ node-test/app.js
@@ -2,3 +2,7 @@ const fileSystem = require('fs');
 
 // Create a new file
 fileSystem.writeFileSync('hello.txt', 'Hello from node js');
+
+const focus = ['Programming', 'Reading', 'Thinking', 'Writing'];
+
+fileSystem.writeFileSync('Focus.txt', focus);
