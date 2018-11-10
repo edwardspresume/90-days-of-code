88444f99f55ffbe29751d5e1f48e92e7fd5dac84
Edwards Presume <edwardspresume@aol.com>
2018-11-10 13:48:26 -0500

    Image rotation

--- Clock/script.js
+++ Clock/script.js
@@ -1,27 +1,27 @@
 const displayClock = () => {
-    const clockDisplay = document.getElementById('clockDisplay');
+  const clockDisplay = document.getElementById("clockDisplay");
 
-    const today = new Date();
-    let session = "AM";
+  const today = new Date();
+  let session = "AM";
 
-    let hour = today.getHours();
-    let minute = today.getMinutes();
-    let seconds = today.getSeconds();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let seconds = today.getSeconds();
 
-    hour = hour < 10 ? '0' + hour : hour;
+  hour = hour < 10 ? "0" + hour : hour;
 
-    if (hour >= 12 && hour < 24) {
-        hour -= 12;
-        session = "PM"
-    }
+  if (hour >= 12 && hour < 24) {
+    hour -= 12;
+    session = "PM";
+  }
 
-    minute = minute < 10 ? '0' + minute : minute;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+  minute = minute < 10 ? "0" + minute : minute;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    let time = `${hour}:${minute}:${seconds} ${session}`;
-    clockDisplay.textContent = time;
+  let time = `${hour}:${minute}:${seconds} ${session}`;
+  clockDisplay.textContent = time;
 
-    setTimeout(displayClock, 1000);
-}
+  setTimeout(displayClock, 1000);
+};
 
 displayClock();
