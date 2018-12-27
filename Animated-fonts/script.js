6fa3cdbee3327eb1a5e4445d339e84e575088738
Edwards Presume <edwardspresume@aol.com>
2018-12-27 16:57:08 -0500

    Refactor

--- Animated-fonts/script.js
+++ Animated-fonts/script.js
@@ -1,62 +1,52 @@
-// DOM elements
-const chain = document.getElementById('chain');
-const battery = document.getElementById('battery');
-const hourglass = document.getElementById('hourglass');
+/* ==========================================================================
+                                 DOM elements
+   ========================================================================== */
 
-// Break chain animation
-const brakeChain = () => {
-    chain.className = 'fas fa-link';
+const chain = document.getElementById("chain");
+const battery = document.getElementById("battery");
+const hourglass = document.getElementById("hourglass");
 
-    setTimeout(() => {
-        chain.className = 'fas fa-unlink';
-    }, 1000);
+/* ==========================================================================
+                            Break chain animation
+   ========================================================================== */
+
+const breakChain = () => {
+    chain.className = "fas fa-link";
+
+    setTimeout(() => (chain.className = "fas fa-unlink"), 1000);
 };
 
-setInterval(brakeChain, 2000);
+breakChain();
+setInterval(breakChain, 2000);
 
-// Charge battery Animation
+/* ==========================================================================
+                        Charge battery Animation
+   ========================================================================== */
+
 const chargeBattery = () => {
+    battery.className = "fas fa-battery-empty";
 
-    battery.className = 'fas fa-battery-empty';
-
-    setTimeout(() => {
-        battery.className = 'fas fa-battery-quarter';
-    }, 1000)
-
-    setTimeout(() => {
-        battery.className = 'fas fa-battery-half';
-    }, 2000)
-
-    setTimeout(() => {
-        battery.className = 'fas fa-battery-three-quarters';
-    }, 3000)
-
-    setTimeout(() => {
-        battery.className = 'fas fa-battery-full';
-    }, 4000)
+    setTimeout(() => (battery.className = "fas fa-battery-quarter"), 1000);
+    setTimeout(() => (battery.className = "fas fa-battery-half"), 2000);
+    setTimeout(() => (battery.className = "fas fa-battery-three-quarters"), 3000);
+    setTimeout(() => (battery.className = "fas fa-battery-full"), 4000);
 };
 
+chargeBattery();
 setInterval(chargeBattery, 5000);
 
-// Hourglass flow Animation
+/* ==========================================================================
+                        Hourglass flow Animation
+   ========================================================================== */
+
 const hourglassFlow = () => {
-    hourglass.className = 'far fa-hourglass';
+    hourglass.className = "far fa-hourglass";
 
-    setTimeout(() => {
-        hourglass.className = 'fas fa-hourglass-start';
-    }, 1000);
+    setTimeout(() => (hourglass.className = "fas fa-hourglass-start"), 1000);
+    setTimeout(() => (hourglass.className = "fas fa-hourglass-half"), 2000);
+    setTimeout(() => (hourglass.className = "fas fa-hourglass-end"), 3000);
+    setTimeout(() => (hourglass.className = "fas fa-hourglass"), 4000);
+};
 
-    setTimeout(() => {
-        hourglass.className = 'fas fa-hourglass-half';
-    }, 2000);
-
-    setTimeout(() => {
-        hourglass.className = 'fas fa-hourglass-end';
-    }, 3000);
-
-    setTimeout(() => {
-        hourglass.className = 'fas fa-hourglass';
-    }, 4000);
-}
-
+hourglassFlow();
 setInterval(hourglassFlow, 5000);
