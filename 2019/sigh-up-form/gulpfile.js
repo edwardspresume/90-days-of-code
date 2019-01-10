7214901a6c6d3d148842c951a098ed99955dddd4
edwardspresume <edwardspresume@aol.com>
2019-01-09 19:22:11 -0500

    Updated development flow

--- 2019/sigh-up-form/gulpfile.js
+++ 2019/sigh-up-form/gulpfile.js
@@ -2,6 +2,7 @@
                                  Packages
    ========================================================================== */
 
+
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
@@ -10,34 +11,73 @@ const concat = require("gulp-concat");
 const htmlmin = require("gulp-htmlmin");
 const imagemin = require("gulp-imagemin");
 const cleanCSS = require("gulp-clean-css");
-const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require("browser-sync").create();
+
+
+/* ==========================================================================
+                                 Package Options
+   ========================================================================== */
+
+const options = {
+  htmlmin: {
+    minifyJS: true,
+    minifyCSS: true,
+    minifyURLs: true,
+    removeComments: true,
+    collapseWhitespace: true,
+    // removeEmptyElements: true,
+    // removeEmptyAttributes: true,
+    removeRedundantAttributes: true
+  },
+
+  cleanCSS: {
+    level: 2
+  },
+
+  imagemin: {
+    optimizationLevel: 5
+  },
+
+  sourcemapsInit: {
+    largeFile: true
+  },
+
+  sourcemapsWrite: {
+    addComment: false
+  }
+};
 
 /* ==========================================================================
                                 File Paths
    ========================================================================== */
 
 const paths = {
-    html: {
-        src: 'src/*.html',
-        dest: "build",
-        files: 'build/*.html'
-    },
+  html: {
+    src: "src/*.html",
+    dest: "build",
+    files: "build/*.html"
+  },
 
-    styles: {
-        src: "src/scss/**/*.scss",
-        dest: "build/css"
-    },
+  styles: {
+    src: "src/scss/**/*.scss",
+    dest: "build/css"
+  },
 
-    scripts: {
-        src: "src/js/*.js",
-        dest: "build/js"
-    },
+  scripts: {
+    src: "src/js/*.js",
+    dest: "build/js"
+  },
 
-    images: {
-        src: "src/images/*",
-        dest: "build/images"
-    }
+  img: {
+    src: "src/img/*",
+    dest: "build/img"
+  },
+
+  sourcemaps: {
+    src: "../maps"
+  }
 };
 
 /* ==========================================================================
@@ -46,78 +86,74 @@ const paths = {
 
 // Copy and minify html files
 const compileMarkup = () => {
-    return gulp
-        .src(paths.html.src)
-        .pipe(
-            htmlmin({
-                collapseWhitespace: true
-            })
-        )
-        .pipe(gulp.dest(paths.html.dest))
-        .pipe(browserSync.stream());
+  return gulp
+    .src(paths.html.src)
+    .pipe(htmlmin(options.htmlmin))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
 };
 
 // Compile sass file to a minimized css file
 const compileStyle = () => {
-    return gulp
-        .src(paths.styles.src)
-        .pipe(sass().on("error", sass.logError))
-        .pipe(autoprefixer())
-        .pipe(
-            cleanCSS({
-                level: 2
-            })
-        )
-        .pipe(rename("main.min.css"))
-        .pipe(gulp.dest(paths.styles.dest))
-        .pipe(browserSync.stream());
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS(options.cleanCSS))
+    .pipe(rename("main.min.css"))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
 };
 
 // Concat minify css files
 const compileScript = () => {
-    return gulp
-        .src(paths.scripts.src)
-        .pipe(concat("main.min.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.scripts.dest))
-        .pipe(browserSync.stream());
+  return gulp
+    .src(paths.scripts.src)
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browserSync.stream());
 };
 
 // Concat and minify js files
-const compileImage = () => {
-    return gulp
-        .src(paths.images.src, {
-            since: gulp.lastRun(compileImage)
-        })
-        .pipe(imagemin({
-            optimizationLevel: 5
-        }))
-        .pipe(gulp.dest(paths.images.dest))
-        .pipe(browserSync.stream());
+const compileImg = () => {
+  return gulp
+    .src(paths.img.src, {
+      since: gulp.lastRun(compileImg)
+    })
+    .pipe(imagemin(options.imagemin))
+    .pipe(gulp.dest(paths.img.dest))
+    .pipe(browserSync.stream());
 };
 
 // BrowserSync
 function startServer() {
-    browserSync.init({
-        server: {
-            baseDir: './build'
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: "./build"
+    }
+  });
 }
 
 // File watcher
 const watchFiles = () => {
-    gulp.watch(paths.html.src, compileMarkup);
-    gulp.watch(paths.styles.src, compileStyle);
-    gulp.watch(paths.scripts.src, compileScript);
-    gulp.watch(paths.html.files).on("change", browserSync.reload);
+  gulp.watch(paths.html.src, compileMarkup);
+  gulp.watch(paths.styles.src, compileStyle);
+  gulp.watch(paths.scripts.src, compileScript);
+  gulp.watch(paths.img.src, compileImg);
+  gulp.watch(paths.html.files).on("change", browserSync.reload);
 };
 
+const compile = gulp.parallel(
+  compileMarkup,
+  compileStyle,
+  compileScript,
+  compileImg
+);
 
-const compile = gulp.parallel(compileMarkup, compileStyle, compileScript, compileImage);
+const serve = gulp.series(compile, startServer);
 
-const serve = gulp.series(compile, startServer)
-
-const run = gulp.parallel(serve, watchFiles)
+const run = gulp.parallel(serve, watchFiles);
 
 gulp.task("default", run);
