890b384d4aa8d077ef4d6b8a44095b13a7ffae6d
edwardspresume <edwardspresume@aol.com>
2019-01-19 18:06:13 -0500

    Intial isPangram solution

--- JavaScript/Practice/codewars/isPangram/isPangram.js
+++ JavaScript/Practice/codewars/isPangram/isPangram.js
@@ -1,13 +1,22 @@
 console.clear();
 
-const isPangram = (string) => {
+const isPangram = (sentence) => {
 
-    for (let i = 0; i < string.length; i++) {
-        let currentChar = string[i];
+    // Store the alphabet as an array
+    let alphabetLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+    // loop through the alphabet array
+    for (let i = 0; i <= alphabetLetters.length - 1; i++) {
 
+        // Store the current letter of the array
+        let letter = alphabetLetters[i];
+
+        // If the given sentence contains a letter from the alphabet, remove the letter from the array;
+        if (sentence.includes(letter)) delete alphabetLetters[i];
     }
 
-    return true;
+    // If the array does not contain any letters return true else false;
+    return alphabetLetters.join('').split('').length === 0
 }
 
+console.log(isPangram('The quick brown fox jumps over the lazy ogg'));
