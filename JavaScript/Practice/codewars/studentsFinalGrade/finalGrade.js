6d52abf3dca7f2af23b2beca12a0e1985a8653bf
edwardspresume <edwardspresume@aol.com>
2019-01-21 15:57:36 -0500

    Final grade

---
+++ JavaScript/Practice/codewars/studentsFinalGrade/finalGrade.js
@@ -0,0 +1,23 @@
+console.clear();
+
+const finalGrade = (examGrade, completedProjects) => {
+
+    if (examGrade >= 90 || completedProjects > 10) return 100;
+    if (examGrade > 75 && completedProjects >= 5) return 90;
+    if (examGrade > 50 && completedProjects >= 2) return 75;
+
+    return 0;
+
+}
+
+console.log(finalGrade(100, 12));
+console.log(finalGrade(90, 0));
+console.log(finalGrade(10, 15));
+
+console.log(finalGrade(85, 5));
+
+console.log(finalGrade(55, 3));
+
+console.log(finalGrade(49, 3));
+
+console.log(finalGrade(20, 3));
