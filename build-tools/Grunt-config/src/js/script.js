276e9d11bce32a8f8046e3811dc32a019aacd62d
edwardspresume <edwardspresume@aol.com>
2019-01-22 09:35:41 -0500

    Lowercased the first name of the folder

---
+++ build-tools/Grunt-config/src/js/script.js
@@ -0,0 +1,2 @@
+console.log('Hello from test 11');
+console.log('Hello from test 22');
---
+++ build-tools/Gulp3-config/src/js/file2.js
@@ -0,0 +1,10 @@
+// Console Log
+
+
+console.log('Log from file 2...');
+
+
+
+// Alert
+
+// alert('This is a message from file 2')
