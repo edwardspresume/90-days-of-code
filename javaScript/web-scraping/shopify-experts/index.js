e2901fa292294224fd32ddc409079a4d313cc46d
edwardspresume <edwardspresume@aol.com>
2019-01-30 13:43:15 -0500

    Improved the async function to load home page

--- javaScript/web-scraping/shopify-experts/index.js
+++ javaScript/web-scraping/shopify-experts/index.js
@@ -1,12 +1,12 @@
-const puppeteer = require("puppeteer");
 const fs = require("fs-extra");
+const puppeteer = require("puppeteer");
 
 (async function main() {
   try {
-    const browser = await puppeteer.launch({ headless: true }); // Launch a browser object for us to surf the web. Headless means no browser GUI
+    const browser = await puppeteer.launch({ headless: true }); // Launch a browser object for us to surf through. Headless means no browser GUI
     const page = await browser.newPage(); // Create a new page for us to navigate to
 
-    async function loadPagSections() {
+    async function loadHomePageSections() {
       await page.goto("https://experts.shopify.com/"); // Go to a specified web site
       await page.waitForSelector(".section"); // Wait for the section elements to load.
       const sections = await page.$$(".section"); // $$ is document.querySelectorAll
@@ -14,38 +14,36 @@ const fs = require("fs-extra");
       return sections;
     }
 
-    const returnedSections = await loadPagSections();
+    const homepageSections = await loadHomePageSections();
 
-    // // Loop through each section of the main page
-    // for (let i = 0; i < returnedSections.length; i++) {
-    //   // await page.goto("https://experts.shopify.com/"); // Go to a specified web site
-    //   // await page.waitForSelector(".section"); // Wait for the section elements to load.
+    // Loop through each section of the home page
+    for (let i = 0; i < homepageSections.length; i++) {
+      const homepageSections = await loadHomePageSections();
+      const section = homepageSections[i];
 
-    //   // const sections = await page.$$(".section"); // $$ is document.querySelectorAll
-    //   returnedSections;
-    //   const section = returnedSections[i];
+      const button = await section.$("a.marketing-button"); // Select the button within each section
+      const buttonName = await page.evaluate(
+        button => button.innerText,
+        button
+      );
 
-    //   const button = await section.$("a.marketing-button"); // Select the button within each section
-    //   const buttonName = await page.evaluate(
-    //     button => button.innerText,
-    //     button
-    //   );
-    //   console.log("\n\n");
-    //   console.log(buttonName);
-    //   button.click();
+      console.log("\n\n");
+      console.log(buttonName);
 
-    //   await page.waitForSelector("#ExpertsResults");
-    //   const lis = await page.$$("li.expert-card");
+      button.click();
 
-    //   // Loop through each LI of the inner page
-    //   for (li of lis) {
-    //     const name = await li.$eval("h2", h2 => h2.innerText);
+      await page.waitForSelector("#ExpertsResults"); // Select the list of the inner pages
+      const lis = await page.$$("li.expert-card"); // Select the list items of the inner pages
 
-    //     console.log(name);
-    //   }
-    // }
-    // // await page.screenshot({ path: "test.png" });
-    // // await browser.close();
+      // Loop through each LI of the inner pages
+      for (li of lis) {
+        const name = await li.$eval("h2", h2 => h2.innerText); // Select the names of each expect
+
+        console.log("Name:", name);
+      }
+    }
+    // await page.screenshot({ path: "test.png" });
+    // await browser.close();
   } catch (err) {
     console.log("our error", err);
   }
